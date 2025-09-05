36c18a034eb82925bb44f5ac1d13c61979ec6727
BhuvanKoduru <83451456+BhuvanKoduru@users.noreply.github.com>
2025-09-05 02:46:21 +0000

    Deploying to gh-pages from @ BhuvanKoduru/BhuvanKoduru.github.io@4d35870e606898bd31eeffb8e169d7de89a8121c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of my projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
+  },{id: "nav-repositories",
           title: "repositories",
           description: "Here are some of my GitHub repositories!",
           section: "Navigation",
